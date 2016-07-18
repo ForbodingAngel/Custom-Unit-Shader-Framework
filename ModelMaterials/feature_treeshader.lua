800f87332c1e2a7797fe57ea8ab81b080186a3d0
ForbodingAngel <forbodingangel@gmail.com>
2016-07-18 15:19:22 -0700

    Catch virtually all vegetation
    This will catch virtually all vegetation of all features made for spring currently. Tested with Spring Features. Additionally, it works with engine trees as well.

--- ModelMaterials/feature_treeshader.lua
+++ ModelMaterials/feature_treeshader.lua
@@ -39,10 +39,65 @@ local materials = {
 
 local featureMaterials = {}
 
--- All feature defs that contain the string "aleppo" will be affected by it
+-- All feature defs that contain the string in ""s will be affected by it
+-- Just to point out that this is a shitty, hacky, gawdawful workaround until I can implement customparam category searching
+
 for id, featureDef in pairs(FeatureDefs) do
-  if featureDef.name:find("aleppo") then
+
+  if featureDef.name:find("bush") then
     featureMaterials[featureDef.name] = "feature_tree"
+  
+  elseif featureDef.name:find("tree") then
+    featureMaterials[featureDef.name] = "feature_tree"
+  
+  elseif featureDef.name:find("vegetation") then
+    featureMaterials[featureDef.name] = "feature_tree"
+	
+  elseif featureDef.name:find("baobab") then
+    featureMaterials[featureDef.name] = "feature_tree"
+	
+  elseif featureDef.name:find("aleppo") then
+    featureMaterials[featureDef.name] = "feature_tree"
+	
+  elseif featureDef.name:find("pine") then
+    featureMaterials[featureDef.name] = "feature_tree"
+	
+  elseif featureDef.name:find("senegal") then
+    featureMaterials[featureDef.name] = "feature_tree"
+	
+  elseif featureDef.name:find("palm") then
+    featureMaterials[featureDef.name] = "feature_tree"
+	
+  elseif featureDef.name:find("shrub") then
+    featureMaterials[featureDef.name] = "feature_tree"
+	
+  elseif featureDef.name:find("bloodthorn") then
+    featureMaterials[featureDef.name] = "feature_tree"
+	
+  elseif featureDef.name:find("birch") then
+    featureMaterials[featureDef.name] = "feature_tree"
+
+  elseif featureDef.name:find("maple") then
+    featureMaterials[featureDef.name] = "feature_tree"
+	
+  elseif featureDef.name:find("oak") then
+    featureMaterials[featureDef.name] = "feature_tree"
+	
+  elseif featureDef.name:find("fern") then
+    featureMaterials[featureDef.name] = "feature_tree"
+	
+  elseif featureDef.name:find("grass") then
+    featureMaterials[featureDef.name] = "feature_tree"
+	
+  elseif featureDef.name:find("weed") then
+    featureMaterials[featureDef.name] = "feature_tree"
+	
+  elseif featureDef.name:find("plant") then
+    featureMaterials[featureDef.name] = "feature_tree"
+  
+  elseif featureDef.name:find("palmetto") then
+    featureMaterials[featureDef.name] = "feature_tree" 
+  
   end
 end
 
